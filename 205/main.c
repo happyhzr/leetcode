b537154672b990b417c2dd52672ba6972447a92c
happy <happy@happys-MacBook-Pro.local>
2020-01-20 21:31:29 +0800

    205

---
+++ 205/main.c
@@ -0,0 +1,54 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+int hash[256];
+
+bool isomorphic(char *s, char *t);
+bool isIsomorphic(char *s, char *t);
+
+bool isomorphic(char *s, char *t)
+{
+    memset(hash, -1, sizeof(hash));
+    int i;
+    int len = strlen(s);
+    for (i = 0; i < len; i++)
+    {
+        int n1 = (int)s[i];
+        int n2 = (int)t[i];
+        if (hash[n1] == -1)
+        {
+            hash[n1] = n2;
+        }
+        else
+        {
+            if (hash[n1] != n2)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool isIsomorphic(char *s, char *t)
+{
+    if (!isomorphic(s, t))
+    {
+        return false;
+    }
+    if (!isomorphic(t, s))
+    {
+        return false;
+    }
+    return true;
+}
+
+int main()
+{
+    char *s = "egg";
+    char *t = "add";
+    bool res = isIsomorphic(s, t);
+    printf("%d\n", res);
+    return 0;
+}
