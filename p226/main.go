14fbeadcb5ee7e607f0adf278c4f68ba1e4f3e45
Happy <insisthzr@163.com>
2022-11-01 21:04:09 +0800

    226t

---
+++ p226/main.go
@@ -0,0 +1,18 @@
+package p226
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	l := invertTree(root.Left)
+	r := invertTree(root.Right)
+	root.Left, root.Right = r, l
+	return root
+}
