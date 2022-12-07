95a2e8d348681e604322388b5ab2d6c46356e5e2
happyhzr <insisthzr@163.com>
2022-12-07 21:10:24 +0800

    268

---
+++ p268/main.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+    want = (0 + nums.length) * (nums.length + 1) / 2
+    got = 0
+    nums.forEach(num => got += num)
+    return want - got
+};
+
+function main() {
+    console.log(missingNumber([3, 0, 1]))
+    console.log(missingNumber([0, 1]))
+    console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
+    console.log(missingNumber([0]))
+}
+
+main()
