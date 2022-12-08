0eb694e4216a425b12573c185f0bb6b5c230710a
happyhzr <insisthzr@163.com>
2022-12-08 21:27:49 +0800

    278

---
+++ p278/main.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        let low = 1
+        let high = n
+        while (low < high) {
+            const mid = Math.floor(low + (high - low) / 2);
+            if (isBadVersion(mid) === false) {
+                low = mid + 1
+            } else {
+                high = mid
+            }
+        }
+        return low
+    };
+};
