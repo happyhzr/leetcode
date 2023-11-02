b4570e5392da15f1c0d1be5652f8d35b2732e151
happyhzr <insisthzr@163.com>
2023-11-02 22:13:03 +0800

    17

--- 17/main.py
+++ 17/main.py
@@ -2,15 +2,8 @@ from typing import List
 
 
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
-        res = []
-        self._helper("", digits, res)
-        return res
-
-    def _helper(self, combination: str, digits: str, res: List[str]) -> None:
-        table = {
+    def __init__(self) -> None:
+        self.table = {
             "2": "abc",
             "3": "def",
             "4": "ghi",
@@ -20,8 +13,17 @@ class Solution:
             "8": "tuv",
             "9": "wxyz",
         }
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        res = []
+        self._helper("", digits, res)
+        return res
+
+    def _helper(self, combination: str, digits: str, res: List[str]) -> None:
         if len(digits) == 0:
             res.append(combination)
         else:
-            for c in table[digits[0]]:
+            for c in self.table[digits[0]]:
                 self._helper(combination + c, digits[1:], res)
