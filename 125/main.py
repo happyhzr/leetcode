499c60750610e70b54c98ca6b3e7357cfde849ac
happyhzr <insisthzr@163.com>
2023-11-03 21:41:31 +0800

    125

--- 125/main.py
+++ 125/main.py
@@ -2,10 +2,6 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         new_s = ""
         for c in s:
-            if ord(c) >= ord("A") and ord(c) <= ord("Z"):
-                new_s += chr(ord(c) + ord("a") - ord("A"))
-            elif ord(c) >= ord("a") and ord(c) <= ord("z"):
-                new_s += c
-            elif ord(c) >= ord("0") and ord(c) <= ord("9"):
-                new_s += c
+            if c.isalnum():
+                new_s += c.lower()
         return new_s == new_s[::-1]
