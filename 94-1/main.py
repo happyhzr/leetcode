47fd6772f0f90acac1287e80bea59415279aa58d
happyhzr <insisthzr@163.com>
2026-03-11 20:30:20 +0800

    94

---
+++ 94-1/main.py
@@ -0,0 +1,21 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode | None) -> list[int]:
+        result = []
+        stack = []
+        current = root
+        while current or stack:
+            if current:
+                stack.append(current)
+                current = current.left
+            else:
+                current = stack.pop()
+                result.append(current.val)
+                current = current.right
+        return result
