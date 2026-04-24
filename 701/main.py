798b27c527efcd0df6ad34a2da38807613920d48
happyhzr <insisthzr@163.com>
2026-04-24 20:05:52 +0800

    701

---
+++ 701/main.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def insertIntoBST(self, root: TreeNode | None, val: int) -> TreeNode | None:
+        if root is None:
+            node = TreeNode(val)
+            return node
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        elif val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
