cfb01d91522b0a434efc364d2864325f3b31eea4
happyhzr <insisthzr@163.com>
2026-07-18 18:44:33 +0800

    122

---
+++ 122/test_main.py
@@ -0,0 +1,8 @@
+from main import Solution
+
+
+def test_maxProfit():
+    s = Solution()
+    assert s.maxProfit([7, 1, 5, 3, 6, 4]) == 7
+    assert s.maxProfit([1, 2, 3, 4, 5]) == 4
+    assert s.maxProfit([7, 6, 4, 3, 1]) == 0
